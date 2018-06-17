4a407c85a9f96b1ee7cde7c4a3c7c8e965940ce4
Merilyn Merisalu <merilyn.merisalu@itcollege.ee>
2018-06-17 20:57:50 +0300

    Ülesande esimene ja teine osa tehtud!

---
+++ Php/Ylesanded/I Ylesanne/1.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+  Ülesanne 1 
+  Merilyn Merisalu
+   17.06.2018
+*/
+
+// Muutujate loomine ja väärtustamine
+
+$nimi="Merilyn Merisalu";
+$vanus=24;
+$sugu="naine";
+
+// Muutujate väljastamine
+
+echo "Minu nimi on $nimi, <br /> olen $vanus aastane <br /> ja minu sugu on $sugu!<br/>";
+echo "<br />";
+// Lause väljustamine
+
+echo '"It\'s Alright, Ma (I\'m Only Bleeding)" - Bob Dylan';
+
+
+?>
