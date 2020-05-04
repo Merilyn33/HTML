2ffabde28a612b13255f5a9eea0f13458646441b
Merilyn Merisalu <merilyn.merisalu@itcollege.ee>
2020-05-04 21:25:12 +0300

    Second session done!

--- JS/CleverProgrammer/Session_1/Scripts/home.js
+++ JS/CleverProgrammer/Session_1/Scripts/home.js
@@ -1,6 +1,24 @@
+//console.log("Hello, JS!");
+//alert("You did it!");
 
-console.log("Hello, JS!");
-alert("You did it!");
+//var name = "Merilyn Merisalu"
+//console.log(name);
 
-var name = "Merilyn Merisalu"
-console.log(name);
+//var sum = 3 + 7;
+//console.log("The answer is: " + sum + "!");
+
+//document.getElementById("someText").innerHTML = "Hello, World!";
+
+var age = prompt("How old are you?");
+document.getElementById("someText").innerHTML = age;
+/*
+    if(age < 18)
+    {
+        document.getElementById("someText").innerHTML = "You are underage!";
+    }
+    else 
+    {
+        
+        document.getElementById("someText").innerHTML = "You are an adult!";
+    }
+*/
